2acf52ba1f562628843535e3d0f5d5d04c11c60f
Hua Chai <huachai.tj@gmail.com>
2015-05-26 08:15:57 +0800

    add login class

---
+++ iOS/Love/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  Love
+//
+//  Created by chaihua on 15/5/21.
+//  Copyright (c) 2015年 love. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController : UIViewController{
+    
+    @IBOutlet weak var NameField: UITextField!
+    @IBOutlet weak var PwdField: UITextField!
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    @IBAction func LoginButtonTouchDownAction(sender: AnyObject) {
+        
+    }
+    
+    override func viewDidLoad() {
+        
+    }
+}
